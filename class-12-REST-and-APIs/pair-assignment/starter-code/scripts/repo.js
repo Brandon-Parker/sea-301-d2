7b420d90078b1eb1220e8a599b585855142ce2a9
BRANDON PARKER <Brandon@dkparker.com>
2016-01-19 14:40:50 -0800

    pair assignment completed by Dave Livingstone, Kate and Brandon P

--- class-12-REST-and-APIs/pair-assignment/starter-code/scripts/repo.js
+++ class-12-REST-and-APIs/pair-assignment/starter-code/scripts/repo.js
@@ -4,9 +4,20 @@
   repos.all = [];
 
   repos.requestRepos = function(callback) {
-    // TODO: How would you like to fetch your repos? Don't forget to call the callback.
+    // DONE: How would you like to fetch your repos? Don't forget to call the callback.
 
-  };
+  $.ajax({
+    url: 'https://api.github.com/users/Brandon-Parker/repos' +
+         '?per_page=5&sort=update',
+    type: 'GET',
+    headers: {'Authorization': 'token ' + githubToken },
+    success: function(data, message, xhr) {
+      console.log(data);
+      repos.all = data;
+    }
+  })
+  .done(callback);
+};
 
   // DONE: Model method that filters the full collection for repos with a particular attribute.
   // You could use this to filter all repos that have a non-zero `forks_count`, `stargazers_count`, or `watchers_count`.
---
+++ class-12-REST-and-APIs/pair-assignment/starter-code/scripts/token.js
@@ -0,0 +1,1 @@
+var githubToken = 'adcea3659ba231d84d8484d886071a5980cb69bd';
